ee245c1b4842cf3e9d1d2d8b1947db1e67a40e1d
Ajitkumarkrimuddichak <kumarajit10897@gmail.com>
2023-05-25 14:30:34 +0530

    E-commerce Frentend

---
+++ src/Contact.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14416.636310321906!2d85.05926373181954!3d25.399485192106646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f2afd3a502a159%3A0x7064e488e1e800fe!2sNadwan%2C%20Bihar%20804452!5e0!3m2!1sen!2sin!4v1676304574548!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/xjvdyjqk" method="POST" className="contact-inputs">
+            <input
+              type="text"
+              placeholder="UserName"
+              name="uesrname"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              required
+              autoComplete="off"
+            />
+            <textarea 
+            name="Message" 
+            cols="30"
+            rows="10"
+            required
+            autoComplete="off"
+            placeholder="Enter you Message"
+            >
+            </textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
